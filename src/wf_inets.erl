544c4c4d725515e680e2478cbd2f4310471de176
Russell Klophaus <rklophaus@rklophaus-mac2.(none)>
2008-11-18 00:15:20 -0500

    Add the wf_inets file.

---
+++ src/wf_inets.erl
@@ -0,0 +1,22 @@
+-module (wf_inets).
+-include ("httpd.hrl").
+-export ([do/1]).
+	
+do(Info) ->
+	{Path, _QueryString} = httpd_util:split_path(Info#mod.request_uri),
+	io:format("Path: ~p~n", [Path]),
+	Module = wf:path_to_module(Path),
+	
+	case Path of
+		"/web" ++ _ -> do(Info, Module);
+		_ -> {proceed, Info#mod.data}
+	end.
+	
+	
+do(Info, Module) ->
+	wf_platform:init(inets, Info),
+	try wf_handle:handle_request(Module)
+	catch Type : Error ->
+		io:format("~p-~p~n~p~n", [Type, Error, erlang:get_stacktrace()]),
+		{proceed, Info#mod.data}
+	end.
