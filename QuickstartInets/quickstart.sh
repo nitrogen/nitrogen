dcceea131035e33e79550b10ef3b9184cef8397a
Russell Klophaus <rklophaus@rklophaus-mac2.local>
2008-11-20 09:53:50 -0500

    Set up quickstart.

--- QuickstartInets/quickstart.sh
+++ QuickstartInets/quickstart.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 export NITROGEN_SRC=..
 cd `dirname $0`
+
+echo Compiling...
+make
+
+echo Creating link to nitrogen support files...
+rm -f wwwroot/nitrogen
+ln -s ../$NITROGEN_SRC/www wwwroot/nitrogen
+
+echo Starting Nitrogen on Inets...
 exec erl \
 	-pa $PWD/ebin $PWD/include \
 	-pa $NITROGEN_SRC/ebin $NITROGEN_SRC/include \
