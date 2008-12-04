d08885e14eb5867d5353ee37b30c872346e7e5e1
Rusty Klophaus <rklophaus@gmail.com>
2008-12-03 19:51:01 -0500

    Simplify web_index.

--- QuickstartInets/src/web_index.erl
+++ QuickstartInets/src/web_index.erl
@@ -7,44 +7,38 @@ main() ->
 	Body = #template { file=twocolumn, title=Title, headline=Title,
 		section1=[
 			#h3 { text="Controls and Validation" },
-			link("Simple Controls", "/web/simplecontrols", web_simplecontrols),
-			link("In-Place Textbox", "/web/advancedcontrols1", web_advancedcontrols1),
-			link("Google Charts", "/web/advancedcontrols2", web_advancedcontrols2),
-			link("Step-by-Step Wizard", "/web/advancedcontrols3", web_advancedcontrols3),
-			link("Validation", "/web/validation", web_validation),
+			#link { text="Simple Controls", url="/web/simplecontrols" }, #br{}, 
+			#link { text="In-Place Textbox", url="/web/advancedcontrols1" }, #br{}, 
+			#link { text="Google Charts", url="/web/advancedcontrols2" }, #br{}, 
+			#link { text="Step-by-Step Wizard", url="/web/advancedcontrols3" }, #br{}, 
+			#link { text="Validation", url="/web/validation" }, #br{}, 
 			#p{},
 		
 			#h3 { text="User Interface" },
-			link("Effects", "/web/effects", web_effects),
-			link("Drag and Drop", "/web/dragdrop", web_dragdrop),
-			link("Sorting", "/web/sorting1", web_sorting1),
-			link("Nested Sorting", "/web/sorting2", web_sorting2)
+			#link { text="Effects", url="/web/effects" }, #br{}, 
+			#link { text="Drag and Drop", url="/web/dragdrop" }, #br{}, 
+			#link { text="Sorting", url="/web/sorting1" }, #br{}, 
+			#link { text="Nested Sorting", url="/web/sorting2" }, #br{}
 		],
 		section2=[
 			#h3 { text="Effects, Events, and Ajax" },
-			link("Postbacks", "/web/postback", web_postback),
-			link("AJAX Replacement", "/web/ajax", web_ajax),
-			link("User Notices", "/web/notices", web_notices),
+			#link { text="Postbacks", url="/web/postback" }, #br{}, 
+			#link { text="AJAX Replacement", url="/web/ajax" }, #br{}, 
+			#link { text="User Notices", url="/web/notices" }, #br{}, 
 			#p{},
 		
 			#h3 { text="Data Binding" },
-			link("Simple (List-Based) Binding", "/web/binding1", web_binding1),
-			link("Record-Based Binding", "/web/binding2", web_binding2),
-			link("Key/Value Pair Binding", "/web/binding3", web_binding3),
-			link("Binding With a Transform Function", "/web/binding4", web_binding4),
+			#link { text="Simple (List-Based) Binding", url="/web/binding1" }, #br{}, 
+			#link { text="Record-Based Binding", url="/web/binding2" }, #br{}, 
+			#link { text="Key/Value Pair Binding", url="/web/binding3" }, #br{}, 
+			#link { text="Binding With a Transform Function", url="/web/binding4" }, #br{}, 
 			#p{},
 		
 			#h3 { text="Advanced Topics" },
-			link("Continuations", "/web/continuations", web_continuations)
+			#link { text="Continuations", url="/web/continuations" }
 		]
 	},
 	wf:render(Body).
-	
-link(Text, Url, _Module) ->
-	[
-		#link { text=Text, url=Url },
-		#br{}
-	].
 	
 event(go) ->
 	wf:flash("Hello there"),
