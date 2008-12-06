d2e00ae33c5e288bc01b945cdd18b775c2a95a8e
Rusty Klophaus <rklophaus@gmail.com>
2008-12-06 13:40:23 -0500

    Added quickstart.bat for Windows. (Hat tip to John Dragos.)

---
+++ Quickstart/quickstart.bat
@@ -0,0 +1,10 @@
+@echo off
+
+echo Copy Nitrogen WWW files into .\content\wwwroot\nitrogen
+rmdir /s /q .\content\wwwroot\nitrogen
+mkdir .\content\wwwroot\nitrogen
+copy ..\www\* .\content\wwwroot\nitrogen
+
+echo Starting Erlang...
+erl -make
+erl -name nitrogen@localhost -pa ebin include -pa ..\ebin ..\include -sync_environment development -s inets_helper
