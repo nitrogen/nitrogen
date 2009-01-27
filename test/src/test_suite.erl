b36102b67ba759d291e4325f81aaa5cf3259eeac
Michael Mullis <michael@mullistechnologies.com>
2009-01-27 13:32:48 -0500

    added more element tests

--- test/src/test_suite.erl
+++ test/src/test_suite.erl
@@ -6,5 +6,14 @@
 
 all_test_() ->
   [{module, element_br_test},
-   {module, element_link_test}
+   {module, element_hr_test},
+   {module, element_image_test},
+   {module, element_label_test},
+   {module, element_link_test},
+   {module, element_list_test},
+   {module, element_listitem_test},
+   {module, element_literal_test},
+   {module, element_p_test},
+   {module, element_span_test},
+   {module, element_value_test}
   ].
