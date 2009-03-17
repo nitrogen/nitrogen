3386961e4ab729e5e40500c46746827ec0626f98
Tom McNulty <tom.mcnulty@cetiforge.com>
2009-03-17 12:27:53 -0600

    Emit value attribute

--- src/elements/forms/element_radio.erl
+++ src/elements/forms/element_radio.erl
@@ -10,27 +10,28 @@ reflect() -> record_info(fields, radio).
 
 render(ControlID, Record) -> 
     CheckedOrNot = case Record#radio.checked of
-		       true -> checked;
-		       _ -> not_checked
-		   end,
+	true -> checked;
+	_ -> not_checked
+    end,
     case Record#radio.postback of
- 	undefined -> ok;
+	undefined -> ok;
 	Postback -> wf:wire(ControlID, #event { type=change, postback=Postback })
     end,
     Content = wf:html_encode(Record#radio.text, Record#radio.html_encode),
     [
-     %% Checkbox...
-     wf_tags:emit_tag(input, [
-			      {id, ControlID}, 
-			      {name, Record#radio.name},
-			      {type, radio},
-			      {class, [radio, Record#radio.class]},
-			      {style, Record#radio.style},
-			      {CheckedOrNot, true}
-			     ]),
-     
-     %% Label for Radio...
-     wf_tags:emit_tag(label, Content, [
-				       {for, ControlID}
-				      ])
+	%% Checkbox...
+	wf_tags:emit_tag(input, [
+		{id, ControlID}, 
+		{name, Record#radio.name},
+		{value, Record#radio.value},
+		{type, radio},
+		{class, [radio, Record#radio.class]},
+		{style, Record#radio.style},
+		{CheckedOrNot, true}
+	    ]),
+
+	%% Label for Radio...
+	wf_tags:emit_tag(label, Content, [
+		{for, ControlID}
+	    ])
     ].
