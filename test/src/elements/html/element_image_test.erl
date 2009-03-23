8d31d8c3870ca3d1579762b72f34178718b89724
Rusty Klophaus <rklophaus@gmail.com>
2009-03-22 22:04:35 -0400

    Update image unit test to account for new 'alt' attribute.

--- test/src/elements/html/element_image_test.erl
+++ test/src/elements/html/element_image_test.erl
@@ -23,6 +23,6 @@ basic_test_() ->
   [?_assertEqual("<img id='1' class='image'/>",new_image_1()),
    ?_assertEqual("<img id='2' class='image t_image'/>",new_image_2()),
    ?_assertEqual("<img id='3' class='image t_image' style='color: cyan;' src='http://an_image.com/sample/image.jpg'/>",new_image_3()),
-   ?_assertEqual([module,id,actions,show_if,class,style,image],
+   ?_assertEqual([module,id,actions,show_if,class,style,image,alt],
 	 element_image:reflect())
   ].
