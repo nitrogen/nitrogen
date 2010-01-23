189eb27e7c1f50df1749785005edc36f7f6f0134
Rusty Klophaus <rklophaus@gmail.com>
2010-01-23 11:01:50 -0500

    Update quickstart.bat for new paths. I don't have a windows machine, so fingers crossed that this works!

--- Quickstart/quickstart.bat
+++ Quickstart/quickstart.bat
@@ -3,8 +3,15 @@
 echo Copy Nitrogen WWW files into .\content\wwwroot\nitrogen
 rmdir /s /q .\wwwroot\nitrogen
 mkdir .\wwwroot\nitrogen
-copy ..\www\* .\wwwroot\nitrogen
+copy ..\apps\nitrogen\www\* .\wwwroot\nitrogen
+
+echo Compile dependencies...
+cd ..\apps\nitrogen
+erl -make
+cd ..\simple_bridge
+erl -make
+cd ..\..\Quickstart
 
 echo Starting Nitrogen on Inets...
 erl -make
-erl -name nitrogen@127.0.0.1 -pa apps ebin include -pa ..\ebin ..\include -eval "application:start(quickstart_inets)"
+erl -name nitrogen@127.0.0.1 -pa apps ebin include ..\apps\nitrogen\ebin ..\apps\nitrogen\include ..\apps\simple_bridge\ebin ..\apps\simple_bridge\include -eval "application:start(quickstart_inets)"
