c53f079d1130ab7fe1f06d7fdfaadea1754e86b5
Rusty Klophaus <rklophaus@gmail.com>
2010-03-04 21:15:23 -0500

    Fix Quickstart to redirect http://localhost:8000/ to http://localhost:8000 web samples.

---
+++ Quickstart/src/index.erl
@@ -0,0 +1,4 @@
+-module(index).
+-compile (export_all).
+
+main() -> wf:redirect("/web/samples").
--- Quickstart/src/web_index.erl
+++
@@ -1,4 +0,0 @@
--module (web_index).
--compile (export_all).
-
-main() -> wf:redirect("/web/samples").
