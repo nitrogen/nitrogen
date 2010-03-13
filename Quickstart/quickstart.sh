c55145a5cad097f324f618731594415343b95c27
Rusty Klophaus <rklophaus@gmail.com>
2010-03-13 09:33:27 -0500

    Continue with redesign.

--- Quickstart/quickstart.sh
+++ Quickstart/quickstart.sh
@@ -8,8 +8,10 @@ cd `dirname $0`
 echo "Starting Nitrogen on Inets (http://localhost:8000)..."
 erl \
     -name nitrogen@127.0.0.1 \
-    -pa ./ebin ../apps/*/ebin ../apps/*/include \
-    -s make all \
-    -eval "application:start(mnesia)" \
-    -eval "application:start(mprocreg)" \
+    -pa ./ebin ../apps/*/ebin ../apps/*/include  \
     -eval "application:start(quickstart)"
+
+#     -env ERL_FULLSWEEP_AFTER 10 \
+#     -eval "application:start(mnesia)" \
+#     -eval "application:start(mprocreg)" \
+#     -eval "application:start(quickstart)"
--- Quickstart/src/index.erl
+++ Quickstart/src/index.erl
@@ -8,18 +8,3 @@ main() -> #template { file="./templates/front.html", bindings=[
 ]}.
 
 title() -> "Nitrogen Web Framework for Erlang".
-headline() -> "".
-
-body() -> [
-	"
-	<div style='margin-left: 22%'>
-	<a href='/web/learn' border=0>
-	<img src=/images/giantleap.png>
-	</a>
-	</div>
-	"
-].
-
-side() -> [].
-	
-event(_) -> ok.
