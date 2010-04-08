7531182ec37ea6bc44c23be31292cfe0703fefd0
Rusty Klophaus <rklophaus@gmail.com>
2010-04-08 07:52:36 -0400

    Update sync to more quickly/intelligently detect Emakefiles.

--- apps/nitrogen/src/sync.erl
+++ apps/nitrogen/src/sync.erl
@@ -12,11 +12,8 @@
 
 %% @doc
 %% Sync is a utility to help you hot-load changed code in an Erlang system.
-%% When you run sync:go(), sync will:
-%% 1. Find all loaded modules that are not system modules.
-%% 2. Search their directories for an Emakefile.
-%% 3. If an Emakefile is found, change to the directory and run make:all([load]).
-
+%% When you run sync:go(), sync will recursively find all Emakefiles under the current directory,
+%% plus all relative code paths, and call make:all([load]) for each one.
 
 
 %% @doc
@@ -25,150 +22,75 @@ go() ->
     go([load]).
 
 
-
 %% @doc
 %% The specified Options are passed into make:all/1.
 go(Options) -> 
-    UserModules = get_user_modules(),
-    Directories = extract_directories(UserModules),
-    run_make_all(Directories, Options).
+    {ok, BaseDir} = file:get_cwd(),
+    SubDirs = get_emakefiles_subdirs(),
+    CodePathDirs = get_emakefiles_codepaths(),
+    Directories = (SubDirs -- CodePathDirs) ++ CodePathDirs,
+    run_make_all(BaseDir, Directories, Options).
 
 
+%% Get directories containing Emakefiles under the current
+%% directory...
+get_emakefiles_subdirs() ->
+    %% Get a reference to all makefile locations...
+    Locations = [".", "*", "*/*", "*/*/*", "*/*/*/*", "*/*/*/*/*"],
+    F = fun(X, Acc) ->
+        filelib:wildcard(X ++ "/Emakefile") ++ Acc
+    end,
+    Files = lists:foldl(F, [], Locations),
 
-%% @private
-%% Get all loaded modules that are NOT system modules.
-get_user_modules() ->
-    % Get a list of all loaded modules...
-    Modules = [X || {X, _} <- code:all_loaded()],
+    %% Convert locations to directories...
+    [filename:dirname(X) || X <- Files].
 
-    % Filter out sticky modules...
-    F1 = fun(X) -> code:is_sticky(X) == false end,
-    Modules1 = lists:filter(F1, Modules),	
-
-    % Filter out system modules...
-    F2 = fun(X, Acc) ->
-        case application:get_key(X, modules) of
-            {ok, L} -> L ++ Acc;
-            undefined -> Acc
+%% Get directories possibly containing Emakefiles referenced in the
+%% codepath.
+get_emakefiles_codepaths() ->
+    F = fun(X, Acc) ->
+        case normalize_codepath(X) of
+            {ok, Path} -> [Path|Acc];
+            undefined  -> Acc
         end
     end,
-    SystemModules = lists:foldl(F2, [], system_applications()),
-    Modules1 -- SystemModules.
+    lists:foldl(F, [], code:get_path()).
 
-
-
-%% @private
-%% Given a list of modules, pull out the src
-%% directory plus one level up.
-extract_directories(Modules) ->
-    F = fun(Module, Acc) ->
-        % Add the src dir plus one level up...
-        case get_src_dir(Module) of
-            undefined -> Acc;
-            SrcDir ->
-                SrcDirs = sets:from_list([SrcDir, filename:dirname(SrcDir)]),
-                sets:union(Acc, SrcDirs)
-        end
-    end,
-    Set = lists:foldl(F, sets:new(), Modules),
-    lists:sort(sets:to_list(Set)).
-
-
+normalize_codepath([$/|_]) -> undefined;
+normalize_codepath(X) ->
+    case lists:reverse(X) of
+        "nibe/" ++ L -> {ok, lists:reverse(L)};
+        _ -> undefined
+    end.
 
 %% @private
 %% Loop through each directory. If an Emakefile is found
 %% then change to that directory and run make:all()
-run_make_all(Dirs, Options) ->
-    {ok, Cwd} = file:get_cwd(),
-    Return = inner_run_make_all(Dirs, Options),
-    file:set_cwd(Cwd),
-    Return.
+run_make_all(BaseDir, Dirs, Options) ->
+    case inner_run_make_all(BaseDir, Dirs, Options) of
+        up_to_date ->
+            file:set_cwd(BaseDir),
+            io:format(" :: Done!~n"),
+            ok;
+        error ->
+            file:set_cwd(BaseDir),
+            io:format(" :: Errors!~n"),
+            error
+    end.
 
-
-
-inner_run_make_all([], _Options) -> up_to_date;
-inner_run_make_all([Dir|Dirs], Options) ->
-    FilePath = filename:join(Dir, "Emakefile"),
-    case filelib:is_file(FilePath) of
+inner_run_make_all(_BaseDir, [], _Options) -> 
+    up_to_date;
+inner_run_make_all(BaseDir, [Dir|Dirs], Options) ->
+    file:set_cwd(filename:join(BaseDir, Dir)),
+    case filelib:is_file("./Emakefile") of
         true ->
-            io:format(":: MAKE - ~s~n", [FilePath]),
-            file:set_cwd(Dir),
+            io:format(":: MAKE - ~s~n", [Dir]),
             case make:all(Options) of
-                up_to_date -> inner_run_make_all(Dirs, Options);
-                error -> error
+                up_to_date -> 
+                    inner_run_make_all(BaseDir, Dirs, Options);
+                error -> 
+                    error
             end;
         false ->
-            inner_run_make_all(Dirs, Options)
+            inner_run_make_all(BaseDir, Dirs, Options)
     end.
-
-
-
-get_src_dir(Module) ->
-    case proplists:get_value(source, Module:module_info(compile), undefined) of
-        undefined -> undefined;
-        Other -> 
-            % io:format("~s - ~s~n", [Module, Other]),
-            filename:dirname(Other)
-    end.
-
-
-
-%% @private
-%% Return a list of system applications. 
-%% Pulled from /usr/local/lib/erlang/lib/*
-system_applications() -> [
-    appmon,
-    asn1,
-    common_test,
-    compiler,
-    cosEvent,
-    cosEventDomain,
-    cosFileTransfer,
-    cosNotification,
-    cosProperty,
-    cosTime,
-    cosTransactions,
-    crypto,
-    debugger,
-    dialyzer,
-    docbuilder,
-    edoc,
-    erl_interface,
-    erts,
-    et,
-    eunit,
-    gs,
-    hipe,
-    ic,
-    inets,
-    inviso,
-    jinterface,
-    kernel,
-    megaco,
-    mnesia,
-    observer,
-    odbc,
-    orber,
-    os_mon,
-    otp_mibs,
-    parsetools,
-    percept,
-    pman,
-    public_key,
-    reltool,
-    runtime_tools,
-    sasl,
-    snmp,
-    ssh,
-    ssl,
-    stdlib,
-    syntax_tools,
-    test_server,
-    toolbar,
-    tools,
-    tv,
-    typer,
-    webtool,
-    wx,
-    xmerl
-].
