4dba8c0f765f81507daa9d45941347060b1361b6
Jesse Gumm <sigmastar@gmail.com>
2012-04-18 15:18:40 -0500

    Remove Docroot from mochiweb. Add body size config

--- rel/overlay/mochiweb/site/src/nitrogen_mochiweb.erl
+++ rel/overlay/mochiweb/site/src/nitrogen_mochiweb.erl
@@ -4,7 +4,7 @@
 
 loop(Req) ->
     {ok, DocRoot} = application:get_env(mochiweb, document_root),
-    RequestBridge = simple_bridge:make_request(mochiweb_request_bridge, {Req, DocRoot}),
+    RequestBridge = simple_bridge:make_request(mochiweb_request_bridge, Req),
     ResponseBridge = simple_bridge:make_response(mochiweb_response_bridge, {Req, DocRoot}),
     nitrogen:init_request(RequestBridge, ResponseBridge),
     nitrogen:run().
