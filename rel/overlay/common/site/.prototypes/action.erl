8a668ce4d2f58e1b316a17a379021e6185b1447b
Jesse Gumm <sigmastar@gmail.com>
2013-08-22 14:02:39 -0500

    Add typespecs to the prototype actions and elements

--- rel/overlay/common/site/.prototypes/action.erl
+++ rel/overlay/common/site/.prototypes/action.erl
@@ -1,12 +1,18 @@
 %% -*- mode: nitrogen -*-
 %% vim: ts=4 sw=4 et
 -module (action_[[[NAME]]]).
--compile (export_all).
 -include_lib ("nitrogen_core/include/wf.hrl").
 -include("records.hrl").
+-export([
+    render_action/1
+]).
 
-%% Move the following line to records.hrl:
--record([[[NAME]]], {?ACTION_BASE(action_[[[NAME]]]), attr1, attr2}).
+%% move the following record definition to site/include/records.hrl
+-record([[[NAME]]], {?ACTION_BASE(action_[[[NAME]]]),
+        attr1 :: any(),
+        attr2 :: any()
+    }).
 
+-spec render_action(#[[[NAME]]]{}) -> actions().
 render_action(_Record = #[[[NAME]]]{}) ->
     "alert('Hello, from [[[NAME]]]!');".
--- rel/overlay/common/site/.prototypes/element.erl
+++ rel/overlay/common/site/.prototypes/element.erl
@@ -1,14 +1,22 @@
 %% -*- mode: nitrogen -*-
 %% vim: ts=4 sw=4 et
 -module (element_[[[NAME]]]).
--compile(export_all).
 -include_lib("nitrogen_core/include/wf.hrl").
 -include("records.hrl").
+-export([
+    reflect/0,
+    render_element/1
+]).
 
-%% Move the following line to records.hrl:
--record([[[NAME]]], {?ELEMENT_BASE(element_[[[NAME]]]), attr1, attr2}).
+%% Move the following record definition to records.hrl:
+-record([[[NAME]]], {?ELEMENT_BASE(element_[[[NAME]]]),
+        attr1 :: any(),
+        attr2 :: any()
+    }).
 
+-spec reflect() -> [atom()].
 reflect() -> record_info(fields, [[[NAME]]]).
 
+-spec render_element(#[[[NAME]]]{}) -> body().
 render_element(_Record = #[[[NAME]]]{}) ->
     "<b>Hello from [[[NAME]]]</b>".
