e114a767af3faffb9b2a099566bd7c87af6bc83c
Jesse Gumm <gumm@sigma-star.com>
2014-10-31 15:11:40 -0500

    Fix default main handler with ws_init

--- rel/overlay/common/site/src/nitrogen_main_handler.erl
+++ rel/overlay/common/site/src/nitrogen_main_handler.erl
@@ -1,11 +1,22 @@
+%% vim: ts=4 sw=4 et
 -module(nitrogen_main_handler).
--export([run/0]).
+-export([
+    run/0,
+    ws_init/0
+]).
+
+handlers() ->
+    %% Put any custom handlers here
+    %% See http://nitrogenproject.com/doc/handlers.html
+    %% Example:
+    %%
+    %%   wf_handler:set_handler(MySecurityHandler, HandlerConfig),
+    %%
+    ok.
+
+ws_init() ->
+    handlers().
 
 run() ->
-	%% Put any custom handlers here
-	%% See http://nitrogenproject.com/doc/handlers.html
-	%% Example:
-	%%
-	%%   wf_handler:set_handler(MySecurityHandler, HandlerConfig),
-	%%
-	wf_core:run().
+    handlers(),
+    wf_core:run().
