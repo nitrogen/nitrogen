078d6e28ff84a00a31e30d44edc211679da347dc
Jesse Gumm <gumm@sigma-star.com>
2015-07-03 22:54:00 -0500

    Update with simple_cache

--- rel/overlay/common/site/src/nitrogen_sup.erl
+++ rel/overlay/common/site/src/nitrogen_sup.erl
@@ -22,6 +22,7 @@ start_link() ->
 %% ===================================================================
 
 init([]) ->
+    application:start(simple_cache),
     application:start(crypto),
     application:start(nprocreg),
     application:start(simple_bridge),
