cfd04fe5ba641105013e612fffb5c9e45685e120
Jesse Gumm <gumm@sigma-star.com>
2021-11-16 12:29:32 -0600

    Add script to add relx to rebar3

--- scripts/convert_to_rebar3.sh
+++ scripts/convert_to_rebar3.sh
@@ -1,23 +1,42 @@
 #!/bin/sh
 
-mv site/src .
-ln -s site/src ../src
-mv site/include .
-ln -s site/include ../include
-rm -fr site/ebin
-rm -fr releases
-rm -fr fix-slim-release
-rm -fr lib
-mv bin/nitrogen bin/nitrogen.old
-mv bin/dev bin/dev.old
-mv Makefile Makefile.old
+mkdir -pv rebar2_to_rebar3_trash
+mkdir -pv priv
+mv -v site/src .
+ln -sv site/src ../src
+mv -v site/include .
+ln -sv site/include ../include
+mv -v site/ebin rebar2_to_rebar3_trash
+mv -v releases rebar2_to_rebar3_trash
+mv -v fix-slim-release rebar2_to_rebar3_trash
+mv -v lib rebar2_to_rebar3_trash
+mv -v rebar rebar2_to_rebar3_trash
+
+mv -v site/static priv
+mv -v site/templates priv
+mv -v bin/nitrogen bin/nitrogen.old
+mv -v bin/dev bin/dev.old
+mv -v Makefile Makefile.old
+ln -sv priv/static site/static
+ln -sv priv/templates site/templates
 
 ## Download new Makefile
 
+curl https://github.com/nitrogen/nitrogen/raw/rebar3/templates/common/Makefile -o Makefile
+
 ## download rebar3
+
+curl https://github.com/nitrogen/nitrogen/raw/rebar3/rebar3 -o "rebar3"
+chmod 755 rebar3
 
 ## download assemble_config.escript
 
+curl https://github.com/nitrogen/nitrogen/raw/rebar3/templates/common/etc/assemble_config.escript -o "etc/assemble_config.escript"
+
 ## download new nitrogen/bin
 
+curl https://github.com/nitrogen/nitrogen/raw/rebar3/templates/common/bin/nitrogen -o "bin/nitrogen"
+chmod 755 bin/nitrogen
+
 ## rewrite rebar.config based on current deps
+./add_relx_to_rebar3.escript
