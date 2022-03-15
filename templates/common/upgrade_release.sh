5caf401f7ef7ae61b26e890c0d06b4ae040758e3
Jesse Gumm <gumm@sigma-star.com>
2022-03-14 19:33:07 -0500

    New copy_static script

--- templates/common/upgrade_release.sh
+++ templates/common/upgrade_release.sh
@@ -7,25 +7,53 @@ CURRENT_VSN=`cat version.current`
 PREVIOUS_VSN=`cat version.previous`
 TARBALL=$APPNAME-$CURRENT_VSN.tar.gz
 
-echo "Generating Release, Relup, and Release Tarball"
-./rebar3 do release, appup generate --previous_version $PREVIOUS_VSN, relup -n $APPNAME -u $PREVIOUS_VSN -v $CURRENT_VSN, tar -o ./releases
+echo "********************************************************************************"
+echo "Generating Release..."
+echo "********************************************************************************"
+./rebar3 release
+
+echo "********************************************************************************"
+echo "Generating Appups..."
+echo "********************************************************************************"
+./rebar3 appup generate --previous_version $PREVIOUS_VSN --current _build/default/rel/$APPNAME
+
+echo "********************************************************************************"
+echo "Generating Relups..."
+echo "********************************************************************************"
+./rebar3 relup -n $APPNAME -u $PREVIOUS_VSN -v $CURRENT_VSN
+
+echo "********************************************************************************"
+echo "Generating Tarball"
+echo "********************************************************************************"
+./rebar3 tar -o ./releases
+
+echo "********************************************************************************"
+echo "Moving the Tarball _build/default/rel/$APPNAME/releases"
+echo "********************************************************************************"
 mv releases/$APPNAME/$TARBALL _build/default/rel/$APPNAME/releases/
 
-echo "Cleaning up releases directory (no longer needed)"
+echo "********************************************************************************"
+echo "Cleaning up releases directory"
+echo "********************************************************************************"
 rm -fr releases
 
-echo "Running the Upgrade: $PREVIOUS_VSN => $CURRENT_VSN"
+echo "********************************************************************************"
+echo "Performing the Upgrade: $PREVIOUS_VSN => $CURRENT_VSN"
+echo "********************************************************************************"
 cd _build/default/rel/$APPNAME
 bin/$APPNAME upgrade $CURRENT_VSN
 
-
-## for some reason, running upgrade removes  the $APPNAME.rel file from the release directory
+## for some reason, running upgrade removes the $APPNAME.rel file from the release directory
 ## these next two lines restore them from the tarball before cleaning them up
+echo "********************************************************************************"
 echo "Extracting $APPNAME.rel from $TARBALL"
+echo "********************************************************************************"
 tar -xvzf releases/$TARBALL releases/$CURRENT_VSN/$APPNAME.rel
 cp -v releases/$CURRENT_VSN/$APPNAME.rel releases/$APPNAME.rel
 
+echo "********************************************************************************"
 echo "Cleaning up $TARBALL"
+echo "********************************************************************************"
 rm -v releases/$TARBALL
 
 
