6aecde601f5ec9df3330e765a6c69a8bc7b4dc9b
Jesse Gumm <gumm@sigma-star.com>
2023-02-11 16:46:46 -0600

    Don't cache in the conversion script

--- scripts/convert_to_rebar3.sh
+++ scripts/convert_to_rebar3.sh
@@ -23,10 +23,11 @@ ln -sv ../priv/static site/static
 ln -sv ../priv/templates site/templates
 
 BASEURL=https://raw.githubusercontent.com/nitrogen/nitrogen/rebar3
+OPTS=-H 'Cache-Control: no-cache, no-store'
 
 ## Download new Makefile
 echo "Downloading $BASEURL/templates/common/Makefile"
-curl $BASEURL/templates/common/Makefile -o Makefile
+curl $OPTS $BASEURL/templates/common/Makefile -o Makefile
 
 ## Replace {{name}} with nitrogen in the Makefile
 echo "Setting the app name to 'nitrogen' in the Makefile"
@@ -34,46 +35,46 @@ sed -i 's/{{name}}/nitrogen/g' Makefile
 
 ## download assemble_config.escript
 echo "Downloading $BASEURL/templates/common/etc/assemble_config.escript"
-curl $BASEURL/templates/common/etc/assemble_config.escript -o "etc/assemble_config.escript"
+curl $OPTS $BASEURL/templates/common/etc/assemble_config.escript -o "etc/assemble_config.escript"
 chmod 755 etc/assemble_config.escript
 
 ## download upgrade_release.sh
 echo "Downloading $BASEURL/templates/common/upgrade_release.sh"
-curl $BASEURL/templates/common/upgrade_release.sh -o "upgrade_release.sh"
+curl $OPTS $BASEURL/templates/common/upgrade_release.sh -o "upgrade_release.sh"
 chmod 755 upgrade_release.sh
 
 ## download copy_static.escript
 echo "Downloading $BASEURL/templates/common/copy_static.escript"
-curl $BASEURL/templates/common/copy_static.escript -o "copy_static.escript"
+curl $OPTS $BASEURL/templates/common/copy_static.escript -o "copy_static.escript"
 chmod 755 copy_static.escript
 
 ## download make_version_file.escript
 echo "Downloading $BASEURL/templates/common/make_version_file.escript"
-curl $BASEURL/templates/common/make_version_file.escript -o "make_version_file.escript"
+curl $OPTS $BASEURL/templates/common/make_version_file.escript -o "make_version_file.escript"
 chmod 755 make_version_file.escript
 
 ## download new nitrogen/bin
 echo "Downloading $BASEURL/templates/common/bin/nitrogen"
-curl $BASEURL/templates/common/bin/nitrogen -o "bin/nitrogen"
+curl $OPTS $BASEURL/templates/common/bin/nitrogen -o "bin/nitrogen"
 chmod 755 bin/nitrogen
 
 ## Get new plugins thing
 echo "Downloading $BASEURL/templates/common/do-plugins.escript"
-curl $BASEURL/templates/common/do-plugins.escript -o "do-plugins.escript"
+curl $OPTS $BASEURL/templates/common/do-plugins.escript -o "do-plugins.escript"
 chmod 755 do-plugins.escript
 
 ## rewrite rebar.config based on current deps
 echo "Downloading $BASEURL/scripts/update_config_to_rebar3.escript"
-curl $BASEURL/scripts/update_config_to_rebar3.escript -o "update_config_to_rebar3.escript"
+curl $OPTS $BASEURL/scripts/update_config_to_rebar3.escript -o "update_config_to_rebar3.escript"
 chmod 755 update_config_to_rebar3.escript
 
 ## Get new plugins.config
 echo "Downloading $BASEURL/templates/common/plugins.config"
-curl $BASEURL/templates/common/plugins.config -o "plugins.config"
+curl $OPTS $BASEURL/templates/common/plugins.config -o "plugins.config"
 
 ## Get erlang_ls.config for Erlang_LS
 echo "Downloading $BASEURL/templates/common/erlang_ls.config"
-curl $BASEURL/templates/common/erlang_ls.config -o "erlang_ls.config"
+curl $OPTS $BASEURL/templates/common/erlang_ls.config -o "erlang_ls.config"
 
 
 echo "Updating rebar.config"
