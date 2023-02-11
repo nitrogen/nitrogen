093ccbcbb202b64f7e967547cc2d1fb1bc601192
Jesse Gumm <gumm@sigma-star.com>
2023-02-11 13:02:18 -0600

    Update convert_to_rebar3 some more

--- scripts/convert_to_rebar3.sh
+++ scripts/convert_to_rebar3.sh
@@ -25,42 +25,53 @@ ln -sv ../priv/templates site/templates
 BASEURL=https://raw.githubusercontent.com/nitrogen/nitrogen/rebar3
 
 ## Download new Makefile
+echo "Downloading $BASEURL/templates/common/Makefile"
 curl $BASEURL/templates/common/Makefile -o Makefile
 
 ## download assemble_config.escript
+echo "Downloading $BASEURL/templates/common/etc/assemble_config.escript"
 curl $BASEURL/templates/common/etc/assemble_config.escript -o "etc/assemble_config.escript"
 chmod 755 etc/assemble_config.escript
 
 ## download upgrade_release.sh
+echo "Downloading $BASEURL/templates/common/upgrade_release.sh"
 curl $BASEURL/templates/common/upgrade_release.sh -o "upgrade_release.sh"
 chmod 755 upgrade_release.sh
 
 ## download copy_static.escript
+echo "Downloading $BASEURL/templates/common/copy_static.escript"
 curl $BASEURL/templates/common/copy_static.escript -o "copy_static.escript"
 chmod 755 copy_static.escript
 
 ## download make_version_file.escript
+echo "Downloading $BASEURL/templates/common/make_version_file.escript"
 curl $BASEURL/templates/common/make_version_file.escript -o "make_version_file.escript"
 chmod 755 make_version_file.escript
 
 ## download new nitrogen/bin
+echo "Downloading $BASEURL/templates/common/bin/nitrogen"
 curl $BASEURL/templates/common/bin/nitrogen -o "bin/nitrogen"
 chmod 755 bin/nitrogen
 
 ## Get new plugins thing
+echo "Downloading $BASEURL/templates/common/do-plugins.escript"
 curl $BASEURL/templates/common/do-plugins.escript -o "do-plugins.escript"
 chmod 755 do-plugins.escript
 
 ## rewrite rebar.config based on current deps
+echo "Downloading $BASEURL/scripts/update_config_to_rebar3.escript"
 curl $BASEURL/scripts/update_config_to_rebar3.escript -o "update_config_to_rebar3.escript"
 chmod 755 update_config_to_rebar3.escript
 
 ## Get new plugins.config
+echo "Downloading $BASEURL/templates/common/plugins.config"
 curl $BASEURL/templates/common/plugins.config -o "plugins.config"
 
 ## Get erlang_ls.config for Erlang_LS
+echo "Downloading $BASEURL/templates/common/erlang_ls.config"
 curl $BASEURL/templates/common/erlang_ls.config -o "erlang_ls.config"
 
+echo "Updating rebar.config for rebar3"
 ./update_config_to_rebar3.escript
 
 ## Add additional new rebar3 rules
